0ef8f83b26789930eff85495aec0a4c174fc4a8f
Tayna08 <taynacarvalhogomes@gmail.com>
2023-05-17 10:30:57 -0400

    adicionando

---
+++ js/conf.js
@@ -0,0 +1,17 @@
+const botoes = document.querySelectorAll("[data-item]");
+botoes.forEach((elemento)=>{
+    elemento.addEventListener("click", (evento)=>{
+        atualizaItem(evento.target.textContent, evento.target.parentNode);
+    });
+    //separação dos botões no "inspecionar"
+});
+//for vai ajudar a percorrer tudo!!!!
+
+function atualizaItem(acao, inputQtde){
+    const item = inputQtde.querySelector("[data-qtde]");
+    if(acao==="-"){
+        item.value = parseInt(item.value)-1;
+    }else{
+        item.value = parseInt(item.value)+1
+    }
+}
